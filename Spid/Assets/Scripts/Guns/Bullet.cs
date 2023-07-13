9207ccad3c8721a5a01ec7584c27b71d1658841a
Raul <raultl6212@gmail.com>
2023-07-13 11:35:14 +0200

    Avance en las balas
    Ya deja trail y particulas cuando choca

--- Spid/Assets/Scripts/Guns/Bullet.cs
+++ Spid/Assets/Scripts/Guns/Bullet.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    private Rigidbody rb;
+    [SerializeField] private int lifeTime;
+    [SerializeField] private GameObject sparks;
+
+    private void Awake(){
+        rb = GetComponent<Rigidbody>();
+    }
+    private void Start(){
+        Invoke("KillBullet", lifeTime);
+    }
+    private void OnCollisionEnter(Collision collision){
+        KillBullet();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void KillBullet(){
+        rb.velocity = Vector3.zero;
+        ParticleSystem particles = sparks.GetComponent<ParticleSystem>();
+        particles.Play();
         
+        Destroy(gameObject, particles.main.duration);
     }
 }
